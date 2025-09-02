7f7659d9f63984cc86054a27760e280e7028913c
Lekhnath <54575651+lekhnath950@users.noreply.github.com>
2025-09-02 12:49:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static export
+  // output: 'export', // enables static export
 };
 
 module.exports = nextConfig;
